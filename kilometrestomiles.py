13f8da0840bde9c81a88847c4a57b015f724cd81
Sameer Husain <husainsameer164@gmail.com>
2025-01-26 17:36:01 +0530

    Kilometres to miles has finished with 0 errors

--- kilometrestomiles.py
+++ kilometrestomiles.py
@@ -2,3 +2,13 @@
     
 if __name__ == "__main__" :
     print("This program will convert Kilometres to miles.")
+    while True:
+        km = (input("Enter a distance in Kilometres: "))
+        if km.isdigit() :
+            km = float(km) 
+            miles = 0.621371 * km
+            print(f"In Kilometres: {km} km")
+            print(f"In Miles: {miles} ml")
+            break
+        else :
+            print("Please enter a valid distance")
